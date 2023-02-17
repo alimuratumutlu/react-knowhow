96443937cc898b3236a3f66a2e74169aff88369f
Murat Umutlu <murat@muum.dev>
2023-02-17 23:35:41 +0300

    added styled components

--- pages/video-layout.tsx
+++ pages/video-layout.tsx
@@ -1,4 +1,20 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  align-items: center;
+  margin: 20px;
+`;
+
+const Square = styled.div`
+  width: 100%;
+  height: 100px;
+  margin-bottom: 10px;
+  background-color: #f0f0f0;
+`;
 
 interface Props {
   objects: any;
@@ -10,22 +26,19 @@ export default function VideoLayout({ objects }: Props) {
 
   return (
     <div>
-      <div>
-        <h2>Staged Objects</h2>
-        <ul>
-          {stagedObjects.map((obj: any) => (
-            <li key={obj.id}>{obj.name}</li>
-          ))}
-        </ul>
-      </div>
-      <div>
-        <h2>Unstaged Objects</h2>
-        <ul>
-          {unstagedObjects.map((obj: any) => (
-            <li key={obj.id}>{obj.name}</li>
-          ))}
-        </ul>
-      </div>
-    </div>
+      <h2>Staged Objects</h2>
+      <Container>
+        {stagedObjects.map((obj: any, index: React.Key | null | undefined) => (
+          <Square key={index} />
+        ))}
+      </Container>
+
+      <h2>Unstaged Objects</h2>
+      <Container>
+        {unstagedObjects.map((obj: any, index: React.Key | null | undefined) => (
+          <Square key={index} />
+        ))}
+      </Container>
+    </div>bn
   );
 }
