0baa053913b304192316f7b5303a522b3fa7c6a1
Murat Umutlu <murat@muum.dev>
2023-03-07 13:15:58 +0300

    algorithm pages added

--- pages/algorithms/introduction.tsx
+++ pages/algorithms/introduction.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
+import Gist from 'react-gist';
+
+import Paragraph from '../../components/atoms/Paragraph/Paragraph.component';
+import SubTitle from '../../components/atoms/SubTitle/SubTitle.component';
+
+import PostTemplate from '../../templates/Post.template';
 
 export default function Introduction() {
-  return (
-    <div>introduction</div>
-  );
+  return <PostTemplate title="Algorithms Introduction"></PostTemplate>;
 }
