f2dcdc8f97dfe61e5f9f877bea35b4f6f9f3753f
Murat Umutlu <murat@muum.dev>
2023-03-08 13:13:34 +0300

    build errors fixed

---
+++ pages/ci-cd-knowledge/continuous-delivery.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function ContinuousDelivery() {
+  return <div>continuous-delivery</div>;
+}
---
+++ templates/ReactHook.template.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function ReactHook() {
+  return <div>ReactHook.template</div>;
+}
